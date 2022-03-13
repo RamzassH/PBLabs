a7c48ae606ecab79030ae054b7f01043cc6cbcdc
RamzAss <romankutcyi123@gmail.com>
2022-03-13 18:06:24 +0300

    реализованы основные сортировки и их модификации

--- libs/experiments_and_measurements/sort_functions/sort_functions.c
+++ libs/experiments_and_measurements/sort_functions/sort_functions.c
@@ -8,10 +8,15 @@ void swap(int *a, int *b) {
     *b = tmp;
 }
 
-/// глупая сортировка, является самым примитивным
-/// представителем обменных сортировок
-/// \param array - сортируемый массив
-/// \param n - количество элементов массива
+void flip(int *a, int m, int n)
+{
+    for(register int i = m; i < --n; i++) {
+        int tmp = a[i];
+        a[i] = a[n];
+        a[n] = tmp;
+    }
+}
+
 void silly_sort(int *array, size_t n) {
     int i = 0;
     size_t countComparison = 0;
@@ -27,10 +32,6 @@ void silly_sort(int *array, size_t n) {
     printf("\nsilly_sort comparison count = %lld\n", countComparison);
 }
 
-/// пузырьковая сортировка.
-/// классическая обменная сортировка
-/// \param array - сортируемый массив
-/// \param n - размер массива
 void bubble_sort(int *array, size_t n) {
     size_t countComparison = 0;
     for (register size_t i = 0; i < n - 1; ++i) {
@@ -44,13 +45,9 @@ void bubble_sort(int *array, size_t n) {
     printf("\nbubble_sort comparison count = %lld\n", countComparison);
 }
 
-/// шейкерная сортировка.
-/// одна из модификаций сортировки пузырьком
-/// \param array - сортируемый массив
-/// \param n - размер массива
 void shaker_sort(int *array, size_t n) {
     int leftPos = 0;
-    int rightPos = n - 1;
+    int rightPos = (int)n - 1;
     int flag = 1;
     size_t countComparison = 0;
     while ((rightPos - leftPos > 0) && (flag > 0)) {
@@ -77,17 +74,13 @@ void shaker_sort(int *array, size_t n) {
     printf("\nshaker_sort comparison count = %lld \n", countComparison);
 }
 
-/// сортировка расческой.
-/// считается лучшей вариацией обменных сортировок
-/// \param array - сортируемый массив
-/// \param n - размер массива
 void comb_sort(int *array, size_t n) {
     size_t gap = n;
     int isSwaped = 1;
     size_t j;
     size_t countComparison = 0;
     while (gap > 1 || isSwaped) {
-        gap = (size_t) (gap / RFACTOR);
+        gap = (size_t) ((double)gap / RFACTOR);
 
         ++countComparison;
         if (gap < 1) {
@@ -110,9 +103,6 @@ void comb_sort(int *array, size_t n) {
 
 //<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-/// сортировка выбором
-/// \param array - сортируемый массив
-/// \param n - размер массива
 void selection_sort(int *array, size_t n) {
     size_t min;
     size_t countComparison = 0;
@@ -130,7 +120,74 @@ void selection_sort(int *array, size_t n) {
     printf("\ncomb_sort comparison count = %lld \n", countComparison);
 }
 
-void dual_sort(int);
+void bingo_sort(int *array, size_t n) {
+    int max = (int)n - 1;
+    int nextValue = array[max];
+
+    size_t countComparison = 0;
+
+    // первый проход
+    for (register int i = max - 1; i >= 0; --i) {
+        ++countComparison;
+        if (array[i] > nextValue) {
+            nextValue = array[i];
+        }
+    }
+
+    while (max && array[max] == nextValue) {
+        ++countComparison;
+        --max;
+    }
+
+    // последующие проходы
+    while (max) {
+        ++countComparison;
+        int value = nextValue;
+        nextValue = array[max];
+        for (int i = max - 1; i >= 0; --i) {
+            ++countComparison;
+            if (array[i] == value) {
+                swap(&array[i], &array[max]);
+                --max;
+            } else if (array[i] > nextValue) {
+                ++countComparison;
+                nextValue = array[i];
+            }
+        }
+        while (max && array[max] == nextValue) {
+            ++countComparison;
+            --max;
+        }
+    }
+
+    printf("\ncomb_sort comparison count = %lld \n", countComparison);
+}
+
+void pancake_sort(int *array, size_t n) {
+    // если длинна массива меньше 2, то сортировка не потребуется
+    if (n < 2) {
+        return;
+    }
+
+    int i;
+    int maxNumPos;
+    for (i = (int)n; i > 1; --i) {
+        maxNumPos = 0;
+        for (register size_t j = 0; j < i; ++j) {
+            if (array[j] > array[maxNumPos]) {
+                maxNumPos = j;
+            }
+        }
+
+        if (maxNumPos == i - 1) {
+            continue;
+        }
+
+        if (maxNumPos >= 0) {
+            flip(array, maxNumPos, i);
+        }
+    }
+}
 
 //<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
@@ -142,7 +199,7 @@ void insertion_sort(int *array, size_t n) {
     for (register size_t i = 1; i < n; ++i) {
         ++countComparison;
         newElement = array[i];
-        location = i - 1;
+        location = (int)i - 1;
 
         while (location  >= 0 && array[location] > newElement) {
             ++countComparison;
@@ -154,4 +211,109 @@ void insertion_sort(int *array, size_t n) {
 
     printf("\ninsertion_sort comparison count = %lld \n", countComparison);
 }
+
+void insertion_binary_sort(int * array, size_t n) {
+    size_t countComparison = 0;
+
+    for (register int i = 0; i < n; ++i) {
+        int key = array[i];
+        int left = 0;
+        int right = i;
+
+        while (right - left > 0) {
+            int mid = left + (right - left) / 2;
+
+            ++countComparison;
+            if (key < array[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        for (int j = i; j >= left + 1; --j) {
+            ++countComparison;
+            array[j] = array[j - 1];
+        }
+        array[left] = key;
+    }
+
+    printf("\ninsertion_binary_sort comparison count = %lld \n", countComparison);
+}
+
+void shell_sort(int *array, size_t n) {
+    size_t countComparison = 0;
+    int interval = (int)n / 2;
+    while (interval > 0) {
+        ++countComparison;
+        for (register int i = interval; i < n; i++) {
+            ++countComparison;
+            int tmp = array[i];
+            int j = i;
+            while (j >= interval && array[j - interval] > tmp) {
+                ++countComparison;
+                array[j] = array[j - interval];
+                j -= interval;
+            }
+            array[j] = tmp;
+        }
+        interval /= 2;
+    }
+    printf("\nshell_sort comparison count = %lld \n", countComparison);
+}
+
+typedef struct {
+    int a;              // данные
+    struct tree *left;  // левый дочерний объект
+    struct tree *right; // правый дочерний объект
+}tree_t;
+
+tree_t *add_to_tree(tree_t *root, int newValue, size_t *countComparison) {
+    (*countComparison)++;
+    if (root == NULL) { // если нет дочерних - создаем новый элемент
+        root = (tree_t*) malloc(sizeof(tree_t));
+        (root)->a = newValue;
+        (root)->left = (root)->right = 0;
+        return root;
+    }
+
+    (*countComparison)++;
+    if ((root)->a < newValue) {             // добавляем новую ветвь
+        (root)->right = add_to_tree((root)->right, newValue, countComparison);
+    } else {
+        (*countComparison)++;
+        (root)->left = add_to_tree((root)->left, newValue, countComparison);
+    }
+
+    return root;
+}
+
+void tree_to_array(tree_t *root, int *a,  size_t *countComparison) {
+    static int max2 = 0;
+    (*countComparison)++;
+    if (root == NULL) {
+        return;
+    }
+
+    tree_to_array((root)->left, a, countComparison);
+    a[max2++] = (root)->a;
+    tree_to_array((root)->right, a, countComparison);
+    free(root);
+}
+
+void tree_sort(int *array, size_t n) {
+    tree_t *root;
+    root = NULL;
+    size_t countComparison = 0;
+    for (int i = 0; i < n; ++i) {
+        ++countComparison;
+        root = add_to_tree(root, array[i], &countComparison);
+    }
+
+    tree_to_array(root, array, &countComparison);
+
+    printf("\ntree_sort comparison count = %lld \n", countComparison);
+}
+
+
 
--- main.c
+++ main.c
@@ -7,7 +7,11 @@
 #include "libs/data_structures/string_t/tasks/task_manager.h"
 #include "libs/data_structures/string_t/test/test_task_string.h"
 #include "libs/experiments_and_measurements/big_O/big_O.h"
+#include "libs/experiments_and_measurements/sort_functions/sort_functions.h"
+
 
 int main() {
-    timeMeasure();
+    int a[] = {5, -3, 0, 10, 1, 5};
+    tree_sort(a, 6);
+    outputArray_(a, 6);
 }
